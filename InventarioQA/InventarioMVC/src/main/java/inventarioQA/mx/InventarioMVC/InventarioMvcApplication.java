a910ca183522c4035269ebd7f6d4ab67231c933d
Tristán Javier Cruz Hernández <124111638+TristanHDZ1@users.noreply.github.com>
2025-05-22 15:01:36 -0600

    Add files via upload

---
+++ InventarioQA/InventarioMVC/src/main/java/inventarioQA/mx/InventarioMVC/InventarioMvcApplication.java
@@ -0,0 +1,13 @@
+package inventarioQA.mx.InventarioMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioMvcApplication.class, args);
+	}
+
+}
---
+++ InventarioQA/InventarioMVC/src/test/java/inventarioQA/mx/InventarioMVC/InventarioMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package inventarioQA.mx.InventarioMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
