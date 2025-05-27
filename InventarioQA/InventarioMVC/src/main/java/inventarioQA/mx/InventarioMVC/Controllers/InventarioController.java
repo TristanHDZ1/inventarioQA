d055e1476c7a6243527926bd05161d057c5d8b91
TristanHDZ1 <tristancruz68@aragon.unam.mx>
2025-05-26 23:11:06 -0600

    Style:Se cambio la interfaz principal y de login

--- InventarioQA/InventarioMVC/src/main/java/inventarioQA/mx/InventarioMVC/Controllers/InventarioController.java
+++ InventarioQA/InventarioMVC/src/main/java/inventarioQA/mx/InventarioMVC/Controllers/InventarioController.java
@@ -1,8 +1,15 @@
 package inventarioQA.mx.InventarioMVC.Controllers;
 
+import inventarioQA.mx.InventarioMVC.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping("/home/")
@@ -12,8 +19,35 @@ public class InventarioController {
     public String Principal() {
         return "Principal";
     }
-    @GetMapping("/Login/")
-    public String Login() {
-        return "Login";
+
+        @Autowired
+        private UsuarioService usuarioService;
+
+
+        @GetMapping("/Login/")
+        public String mostrarLogin() {
+            return "Login";
+        }
+
+        @PostMapping("/Login/")
+        public String procesarLogin(@RequestParam String usuario,
+                                    @RequestParam String password,
+                                    HttpSession session,
+                                    Model model) {
+            if (usuarioService.autenticar(usuario, password)) {
+                session.setAttribute("usuario", usuario);
+                return "redirect:/dashboard";
+            } else {
+                model.addAttribute("error", "Usuario o contraseña inválidos");
+                return "login";
+            }
+        }
+
+        @GetMapping("/dashboard/")
+        public String verDashboard() {
+            return "dashboard"; // Página después del login
+        }
+
+
     }
-}
+
--- InventarioQA/InventarioMVC/src/main/java/inventarioQA/mx/InventarioMVC/Models/Usuario.java
+++ InventarioQA/InventarioMVC/src/main/java/inventarioQA/mx/InventarioMVC/Models/Usuario.java
@@ -2,6 +2,7 @@ package inventarioQA.mx.InventarioMVC.Models;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "usuarios")
@@ -17,45 +18,70 @@ public class Usuario implements Serializable {
     @Column(nullable = false, length = 255)
     private String password;
 
-    public Usuario() {}
+    @Column(nullable = false)
+    private String rol; // ejemplo: "ROLE_ADMIN" o "ROLE_USER"
 
-    public Usuario(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public Usuario() {
     }
 
-    // Getters
+    public Usuario(Long id, String username, String password, String rol) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    // Setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    // toString opcional
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id) && Objects.equals(username, usuario.username) && Objects.equals(password, usuario.password) && Objects.equals(rol, usuario.rol);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, rol);
+    }
+
     @Override
     public String toString() {
         return "Usuario{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", rol='" + rol + '\'' +
                 '}';
     }
 }
